b7e009d6886d71af326ee0bab1ca5776bbdb60ff
Borislav Stoyanov <bostoycontact@gmail.com>
2022-02-13 17:57:03 +0100

    Initialize project

---
+++ src/main/java/com/appointr/AppointrApplication.java
@@ -0,0 +1,13 @@
+package com.appointr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appointr/AppointrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appointr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
