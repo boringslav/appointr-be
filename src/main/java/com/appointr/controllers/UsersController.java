40eb8510da95399fe47d750abff336ea44569fa6
Stoyanov,Borislav B <b.stoyanov@student.fontys.nl>
2022-03-11 13:47:08 +0100

    Add a business / service layer

--- src/main/java/com/appointr/controllers/UsersController.java
+++ src/main/java/com/appointr/controllers/UsersController.java
@@ -2,6 +2,7 @@ package com.appointr.controllers;
 
 import com.appointr.model.User;
 import com.appointr.repository.FakeDataStore;
+import com.appointr.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,15 +13,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UsersController {
-    private FakeDataStore fakeDataStore;
+
+
     @Autowired
-    public UsersController(FakeDataStore fakeDataStore) {
-        this.fakeDataStore = fakeDataStore;
-    }
+    private UserService userService;
 
     @GetMapping
     public ResponseEntity<List<User>> getUsers() {
-        List<User> users = fakeDataStore.getUsers();
+        List<User> users = userService.getUsers();
         if (users.size() > 0) {
             return ResponseEntity.ok().body(users);
         } else {
@@ -30,7 +30,7 @@ public class UsersController {
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable(value = "id") String id) {
-        User user = fakeDataStore.getUser(id);
+        User user = userService.getUser(id);
         if (user != null) {
             return ResponseEntity.ok().body(user);
         } else {
