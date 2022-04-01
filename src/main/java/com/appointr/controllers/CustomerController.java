3a850bcfeeb304590964c9b2d787612750e6ff28
Stoyanov,Borislav B <b.stoyanov@student.fontys.nl>
2022-04-01 08:46:02 +0200

    Create getCustomerById request

--- src/main/java/com/appointr/controllers/CustomerController.java
+++ src/main/java/com/appointr/controllers/CustomerController.java
@@ -1,24 +1,40 @@
 package com.appointr.controllers;
 
 
+import com.appointr.dto.customer.CustomerDTO;
 import com.appointr.dto.customer.GetAllCustomersResponseDTO;
-import com.appointr.repository.CustomerReposity;
+import com.appointr.services.customer.GetCustomerById;
 import com.appointr.services.customer.GetCustomers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
     private final GetCustomers getCustomers;
+    private final GetCustomerById getCustomerById;
 
-    public CustomerController(GetCustomers getCustomers) {
+    public CustomerController(GetCustomers getCustomers,GetCustomerById getCustomerById) {
         this.getCustomers = getCustomers;
+        this.getCustomerById =getCustomerById;
     }
 
     @GetMapping
     public ResponseEntity<GetAllCustomersResponseDTO> getAllCustomers() {
         return ResponseEntity.ok(getCustomers.getAllCustomers());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable(value="id") final long id) {
+        final Optional<CustomerDTO> customer = getCustomerById.getCustomer(id);
+        if(customer.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(customer.get());
+    }
+
+
 }
