3569751e8c96c08d4de45eca1def4112aaf2e3de
Stoyanov,Borislav B <b.stoyanov@student.fontys.nl>
2022-04-01 07:35:25 +0200

    Create Customer DTO

---
+++ src/main/java/com/appointr/dto/customer/CustomerDTO.java
@@ -0,0 +1,17 @@
+package com.appointr.dto.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDTO {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
