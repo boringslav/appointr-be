024a3c02ddcfa65cc42a283f89aa44457914d32d
Stoyanov,Borislav B <b.stoyanov@student.fontys.nl>
2022-04-01 07:35:53 +0200

    Create Get Customers Response DTO

---
+++ src/main/java/com/appointr/dto/customer/GetAllCustomersResponseDTO.java
@@ -0,0 +1,16 @@
+package com.appointr.dto.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAllCustomersResponseDTO {
+    private List<CustomerDTO> customers;
+}
