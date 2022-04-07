4d067fbec15809df7ce43bd1fc01f62358a99d79
Borislav Stoyanov <bostoy@protonmail.com>
2022-04-07 17:29:25 +0200

    Create Customer SignUpResponse DTO

---
+++ src/main/java/com/appointr/dto/customer/CustomerSignUpResponseDTO.java
@@ -0,0 +1,17 @@
+package com.appointr.dto.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerSignUpResponseDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
