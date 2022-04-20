d385fa24b9f3cc81deaf61b8bc1dd2e8779bb5b8
Borislav Stoyanov <bostoy@protonmail.com>
2022-04-20 14:13:57 +0200

    Fix BookingController CORS

--- src/main/java/com/appointr/controllers/BookingController.java
+++ src/main/java/com/appointr/controllers/BookingController.java
@@ -12,7 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
-@CrossOrigin
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/bookings")
 @AllArgsConstructor
