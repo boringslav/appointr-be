6723d4ff859d02a7a2aa0fa167d238fcc1730145
Borislav Stoyanov <bostoy@protonmail.com>
2022-04-24 14:19:21 +0200

    Create Spring Security Configuration

--- src/main/java/com/appointr/services/user/impl/UserServiceImpl.java
+++ src/main/java/com/appointr/services/user/impl/UserServiceImpl.java
@@ -10,18 +10,38 @@ import com.appointr.repository.entity.UserRole;
 import com.appointr.services.user.UserService;
 import com.google.common.collect.Streams;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
-public class UserServiceImpl implements UserService {
+@Transactional
+
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
 
-    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(email);
+        if(user == null) {
+            throw new UsernameNotFoundException("User not found in the database");
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole().toString()));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+
     public UserSignUpResponseDTO signUp(UserSignUpRequestDTO requestDTO) {
         User newUser = User.builder()
                 .email(requestDTO.getEmail())
