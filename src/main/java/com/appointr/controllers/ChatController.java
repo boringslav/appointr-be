55ec8381bf236959beac9499b301ae0e52e39e44
Borislav Stoyanov <borislav.stoyanov@musala.com>
2022-06-12 22:45:48 +0200

    Edit Security so every user can see his own profile

--- src/main/java/com/appointr/controllers/ChatController.java
+++ src/main/java/com/appointr/controllers/ChatController.java
@@ -2,7 +2,6 @@ package com.appointr.controllers;
 
 import com.appointr.ws.model.Message;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @Slf4j
 public class ChatController {
 
-    @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/message")
