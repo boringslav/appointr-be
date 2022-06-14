934c2c68eb89c770ce1473d81aace07a61fa9b76
Borislav Stoyanov <borislav.stoyanov@musala.com>
2022-06-14 17:41:11 +0200

    Add More tests

--- src/test/java/com/appointr/controllers/UserControllerTest.java
+++ src/test/java/com/appointr/controllers/UserControllerTest.java
@@ -1,8 +1,8 @@
 package com.appointr.controllers;
 
-import com.appointr.dto.user.GetAllUsersResponseDTO;
-import com.appointr.dto.user.UserDTO;
+import com.appointr.dto.user.*;
 import com.appointr.repository.entity.User;
+import com.appointr.repository.entity.UserRole;
 import com.appointr.services.user.UserService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -10,15 +10,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -46,13 +49,13 @@ class UserControllerTest {
         mockMvc.perform(get("/users")).andExpect(status().isForbidden());
     }
 
-    @Disabled
     @Test
+    @Disabled
     @WithMockUser(username = "borkotest@gmail.com", roles = {"ADMIN"})
     void getAllUsers_shouldBeOK_whenRoleIsAdmin() throws Exception {
         when(userService.getAllUsers()).thenReturn(GetAllUsersResponseDTO.builder().build());
 
-        mockMvc.perform(get("/users")).andExpect(status().isOk());
+        mockMvc.perform(get("/users").contentType("application/json")).andExpect(status().isOk());
     }
 
     @Test
@@ -71,11 +74,77 @@ class UserControllerTest {
 
 
     @Test
-    void signUp() {
+    @WithMockUser(username = "borkotest@gmail.com", roles = {"COMPANY"}, password = "password")
+    void signUp_Should_returnCreated_WhenProvidedWithCorrectCredentials() throws Exception {
+
+        when(userService.signUp(UserSignUpRequestDTO.builder()
+                .email("borkotest@gmail.com")
+                .password("password")
+                .role(UserRole.COMPANY)
+                .build())).thenReturn(UserSignUpResponseDTO.builder().build());
+
+        mockMvc.perform(post("/users/sign-up")
+                        .contentType("application/json")
+                        .content("""
+                                {
+                                	"email": "borkotest@gmail.com",
+                                	"password": "password",
+                                	"name": "BorkoCorp",
+                                	"role": "COMPANY"
+                                }
+                                """
+                ))
+                .andExpect(status().isCreated());
+    }
+    @Test
+    @WithMockUser(username = "borkotest@gmail.com", roles = {"CUSTOMER"}, password = "password")
+    void signUp_Should_returnBadRequest_WhenNotProvidedWithCorrectCredentials() throws Exception {
+        when(userService.signUp(UserSignUpRequestDTO.builder()
+                .email("borkotest@gmail.com")
+                .password("password")
+                .role(UserRole.CUSTOMER)
+                .build())).thenReturn(UserSignUpResponseDTO.builder().build());
+
+        mockMvc.perform(post("/users/sign-up")
+                        .contentType("application/json")
+                        .content("""
+                                {
+                                	"email": "borkotest@gmail.com",
+                                	"password": "password",
+                                }
+                                """
+                        ))
+                .andExpect(status().isBadRequest());
     }
 
+    @Disabled
     @Test
-    void updateMe() {
+    @WithMockUser(username = "borkotest@gmail.com", roles = {"CUSTOMER"}, password = "password")
+    void updateMe_should_UpdateTheLoggedInUserDetails() throws Exception {
+        EditUserDTORequest editUserDTORequest = EditUserDTORequest.builder()
+                .email("borkotest1gmail.com")
+                .name("borko")
+                .password("123123")
+                .build();
+
+        when(userService.editMyProfile(editUserDTORequest)).thenReturn(
+                UserDTO.builder()
+                        .id(1L)
+                        .name("borko")
+                        .email("borkotest1gmail.com")
+                        .role(UserRole.COMPANY)
+                        .build()
+        );
+        mockMvc.perform(put("/users/edit-me")
+                        .contentType("application/json")
+                        .content("""
+                                {
+                                	"email": "borkotest1gmail.com",
+                                	"password": "password",
+                                	"name": "burger",
+                                }
+                                """))
+                .andExpect(status().isCreated());
     }
 
     @Test
